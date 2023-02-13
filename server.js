efef6b4179883183b77c3097170576883af4196e
Ben C <greenboyb1@gmail.com>
2023-02-13 15:53:58 -0500

    ts

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ var credentials = {key: privateKey, cert: certificate};
 const app = express()
 app.use(cors())
 app.use(createProxyMiddleware({
-  router: (req) => console.log (new URL(req.url.substring(1)).pathname + new URL(req.url.substring(1)).search)/*,
-  pathRewrite: (path, req) => (new URL(req.url.substring(1))).pathname + new URL(req.url.substring(1)).search),
+  router: (req) => new URL(req.url.substring(1)),
+  pathRewrite: (path, req) => (new URL(req.url.substring(1)).pathname + new URL(req.url.substring(1)).search,
   changeOrigin: true,
-  logger: console*/
+  logger: console
 }))
 
 https.createServer(credentials, app).listen(port);
