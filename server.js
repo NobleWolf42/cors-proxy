5627ed9d27a2e8de247c6909039b8d372acd1594
Ben C <greenboyb1@gmail.com>
2023-02-13 15:57:56 -0500

    ts

--- server.js
+++ server.js
@@ -9,13 +9,40 @@ var privateKey  = readFileSync(botConfig.oauth.privateKey, 'utf8');
 var certificate = readFileSync(botConfig.oauth.publicKey, 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
-const app = express()
-app.use(cors())
-app.use(createProxyMiddleware({
-  router: (req) => new URL(req.url.substring(1)),
-  pathRewrite: (path, req) => (new URL(req.url.substring(1))).pathname + new URL(req.url.substring(1)).search,
-  changeOrigin: true,
-  logger: console
-}))
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '1000kb';
+console.log('Using limit: ', myLimit);
+
+app.use(bodyParser.json({limit: myLimit}));
+
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
+});
 
 https.createServer(credentials, app).listen(port);
