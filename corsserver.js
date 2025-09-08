a905d8b7521224070ecb90fd558d12dabf225cc3
NobleWolf42 <greenboyb1@gmail.com>
2025-09-08 15:59:02 -0400

    added logging

--- corsserver.js
+++ corsserver.js
@@ -107,6 +107,7 @@ app.all("*", function (req, res, next) {
                         if (error) {
                             console.error("error: " + response.statusCode);
                         }
+                        console.log(body);
                     }
                 ).pipe(res);
             }
