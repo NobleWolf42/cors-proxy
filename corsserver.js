8c12e6cce5884e5ad7ba34991d01191c5c7d1118
NobleWolf42 <greenboyb1@gmail.com>
2025-09-30 09:00:42 -0400

    testing rebulid

--- corsserver.js
+++ corsserver.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const botConfig = require("./botconfig.json");
 const port = botConfig.oauth.port;
+let statsFile = require("./stats.json");
+let stats = JSON.parse(statsFile);
 
 var express = require("express"),
     request = require("request"),
@@ -13,7 +15,7 @@ console.log("Using limit: ", myLimit);
 
 app.use(bodyParser.json({ limit: myLimit }));
 
-app.all("*", function (req, res, next) {
+app.all("*", function (req, res) {
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     const origin = req.get("origin");
     if (
@@ -31,16 +33,61 @@ app.all("*", function (req, res, next) {
     if (req.method === "OPTIONS") {
         // CORS Preflight
         res.status(200).send();
-    } else {
-        const splitURL = req.url.split("/");
-        if (splitURL[1] == "steamimages") {
+    }
+});
+
+app.post("/steamimages", function (req, res) {
+    const splitURL = req.url.split("/");
+    request(
+        {
+            url:
+                "https://media.steampowered.com/steamcommunity/public/images/apps/" +
+                splitURL[2] +
+                "/" +
+                splitURL[3],
+            method: req.method,
+        },
+        function (error, response) {
+            if (error) {
+                console.error("error: " + response.statusCode);
+            }
+        }
+    ).pipe(res);
+});
+
+app.post("/", function (req, res) {
+    const targetURL = req.header("Target-URL");
+    if (targetURL == "steam") {
+        if (req.header("steamId") != undefined) {
+            console.log(req.header("steamId"));
             request(
                 {
                     url:
-                        "https://media.steampowered.com/steamcommunity/public/images/apps/" +
-                        splitURL[2] +
-                        "/" +
-                        splitURL[3],
+                        "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" +
+                        botConfig.steam.key +
+                        "&steamid=" +
+                        req.header("steamId") +
+                        "&format=json&include_appinfo=1" +
+                        req.url,
+                    method: req.method,
+                },
+                function (error, response, body) {
+                    if (error) {
+                        console.error("error: " + response.statusCode);
+                    }
+                }
+            ).pipe(res);
+        } else if (req.header("steamUsername") != undefined) {
+            console.log(req.header("steamUsername"));
+            request(
+                {
+                    url:
+                        "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" +
+                        botConfig.steam.key +
+                        "&vanityurl=" +
+                        req.header("steamUsername") +
+                        "&format=json&include_appinfo=1" +
+                        req.url,
                     method: req.method,
                 },
                 function (error, response, body) {
@@ -50,86 +97,63 @@ app.all("*", function (req, res, next) {
                 }
             ).pipe(res);
         }
-        const targetURL = req.header("Target-URL");
-        if (targetURL == "steam") {
-            if (req.header("steamId") != undefined) {
-                console.log(req.header("steamId"));
-                request(
-                    {
-                        url:
-                            "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" +
-                            botConfig.steam.key +
-                            "&steamid=" +
-                            req.header("steamId") +
-                            "&format=json&include_appinfo=1" +
-                            req.url,
-                        method: req.method,
-                    },
-                    function (error, response, body) {
-                        if (error) {
-                            console.error("error: " + response.statusCode);
-                        }
+    } else if (targetURL == "dictionaryDef") {
+        if (req.header("word") != undefined) {
+            console.log(req.header("word"));
+            request(
+                {
+                    url:
+                        "https://www.merriam-webster.com/dictionary/" +
+                        req.header("word"),
+                    method: req.method,
+                },
+                function (error, response, body) {
+                    if (error) {
+                        console.error("error: " + response.statusCode);
                     }
-                ).pipe(res);
-            } else if (req.header("steamUsername") != undefined) {
-                console.log(req.header("steamUsername"));
-                request(
-                    {
-                        url:
-                            "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" +
-                            botConfig.steam.key +
-                            "&vanityurl=" +
-                            req.header("steamUsername") +
-                            "&format=json&include_appinfo=1" +
-                            req.url,
-                        method: req.method,
-                    },
-                    function (error, response, body) {
-                        if (error) {
-                            console.error("error: " + response.statusCode);
-                        }
+                    console.log(body);
+                }
+            ).pipe(res);
+        }
+    } else if (targetURL == "dictionaryAPI") {
+        if (req.header("word") != undefined) {
+            console.log(req.header("word"));
+            request(
+                {
+                    url:
+                        "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" +
+                        req.header("word") +
+                        "?key=" +
+                        botConfig.dictionary.key,
+                    method: req.method,
+                },
+                function (error, response, body) {
+                    if (error) {
+                        console.error("error: " + response.statusCode);
                     }
-                ).pipe(res);
-            }
-        } else if (targetURL == "dictionaryDef") {
-            if (req.header("word") != undefined) {
-                console.log(req.header("word"));
-                request(
-                    {
-                        url:
-                            "https://www.merriam-webster.com/dictionary/" +
-                            req.header("word"),
-                        method: req.method,
-                    },
-                    function (error, response, body) {
-                        if (error) {
-                            console.error("error: " + response.statusCode);
-                        }
-                        console.log(body);
-                    }
-                ).pipe(res);
-            }
-        } else if (targetURL == "dictionaryAPI") {
-            if (req.header("word") != undefined) {
-                console.log(req.header("word"));
-                request(
-                    {
-                        url:
-                            "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" +
-                            req.header("word") +
-                            "?key=" +
-                            botConfig.dictionary.key,
-                        method: req.method,
-                    },
-                    function (error, response, body) {
-                        if (error) {
-                            console.error("error: " + response.statusCode);
-                        }
-                    }
-                ).pipe(res);
-            }
+                }
+            ).pipe(res);
         }
     }
+});
+
+app.post("/getstats", function (req, res) {
+    stats.viewCounts.CSArtifact += 1;
+    jsondata = JSON.stringify(stats);
+    fs.writeFile("./stats.json", jsondata, function (err) {
+        if (err) {
+            console.log("Save File Failed.");
+            console.log(err);
+            response.json({
+                success: false,
+            });
+        } else {
+            console.log("File Saved Successfully!");
+            response.json({
+                success: true,
+            });
+        }
+    });
 });
 
 http.createServer(app).listen(port);
